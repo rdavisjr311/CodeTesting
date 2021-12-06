c3e07bfe0eaec65da60022d35042b5f76f73e301
rdavisjr311 <bob.davis@kensci.com>
2021-12-06 16:11:00 -0500

    test

---
+++ input.sh
@@ -0,0 +1,9 @@
+echo "Enter Date (Format: mm/dd/yyyy"
+read a
+echo "Enter Customer"
+read x
+echo "Enter Tenant"
+read y
+echo "Enter Enviroment"
+read z
+echo "--global.customer ${x} --global.tenant ${y} --global.environment ${z}"
