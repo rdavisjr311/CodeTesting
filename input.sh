02ca23ceac3d1d95145a2a6724bb963080dad622
rdavisjr311 <bob.davis@kensci.com>
2021-12-08 09:36:41 -0500

    test

--- input.sh
+++ input.sh
@@ -1,17 +1,13 @@
-custenv () {
-    customer=
-    tenant=
-    environment=
-    clear
-    echo "Enter Customer"
-    read customer
-    clear
-    echo "Enter Tenant"
-    read tenant
-    clear
-    echo "Enter Enviroment"
-    read environment
-}
+customer=
+tenant=
+environment=
+echo "Enter Customer"
+read customer
+echo "Enter Tenant"
+read tenant
+echo "Enter Enviroment"
+read environment
+
 echo "Is this the correct information? (y/n)"
 echo "--global.customer $customer --global.tenant $tenant --global.environment $environment"
 read answer
@@ -19,5 +15,5 @@ if [ $answer=y ];
     then
         echo "--global.customer $customer --global.tenant $tenant --global.environment $environment"
     else
-        custenv
+        echo "Not correct information."
 fi
