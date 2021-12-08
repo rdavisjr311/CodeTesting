5121fc320d034d215cb6ad48c95922283f9ca59a
rdavisjr311 <bob.davis@kensci.com>
2021-12-08 09:23:52 -0500

    change

--- input.sh
+++ input.sh
@@ -1,13 +1,23 @@
-customer=
-tenant=
-environment=
-clear
-echo "Enter Customer"
-read customer
-clear
-echo "Enter Tenant"
-read tenant
-clear
-echo "Enter Enviroment"
-read environment
+custenv () {
+    customer=
+    tenant=
+    environment=
+    clear
+    echo "Enter Customer"
+    read customer
+    clear
+    echo "Enter Tenant"
+    read tenant
+    clear
+    echo "Enter Enviroment"
+    read environment
+}
+echo "Is this the correct information? (y/n)"
 echo "--global.customer $customer --global.tenant $tenant --global.environment $environment"
+read answer
+if [ $answer=y ]; 
+    then
+        echo "--global.customer $customer --global.tenant $tenant --global.environment $environment"
+    else
+        custenv
+fi
