61c18fe98097dcd7ed0cb3213d2f26c1ba05b750
rdavisjr311 <bob.davis@kensci.com>
2021-12-08 08:51:52 -0500

    update

--- input.sh
+++ input.sh
@@ -1,10 +1,13 @@
+customer=
+tenant=
+environment=
 clear
 echo "Enter Customer"
-read x
+read customer
 clear
 echo "Enter Tenant"
-read y
+read tenant
 clear
 echo "Enter Enviroment"
-read z
-echo "--global.customer ${x} --global.tenant ${y} --global.environment ${z}"
+read environment
+echo "--global.customer $customer --global.tenant $tenant --global.environment $environment"
