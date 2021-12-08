55ceb6b78db493a7aad9de3303b6d41f906f469f
rdavisjr311 <bob.davis@kensci.com>
2021-12-08 15:01:20 -0500

    testing

--- input.sh
+++ input.sh
@@ -1,12 +1,19 @@
+gather_vars {
+    clear
+    echo "Enter Customer"
+    read customer
+    clear
+    echo "Enter Tenant"
+    read tenant
+    clearn
+    echo "Enter Enviroment"
+    read environment
+}
 customer=
 tenant=
 environment=
-echo "Enter Customer"
-read customer
-echo "Enter Tenant"
-read tenant
-echo "Enter Enviroment"
-read environment
+
+gather_vars
 
 echo "Is this the correct information? (y/n)"
 echo "--global.customer $customer --global.tenant $tenant --global.environment $environment"
