928459e8bbf7b04900220dae451b2d6bc4c4dfbd
rdavisjr311 <bob.davis@kensci.com>
2021-12-08 08:47:47 -0500

    update clear statements

--- input.sh
+++ input.sh
@@ -1,7 +1,10 @@
+clear
 echo "Enter Customer"
 read x
+clear
 echo "Enter Tenant"
 read y
+clear
 echo "Enter Enviroment"
 read z
 echo "--global.customer ${x} --global.tenant ${y} --global.environment ${z}"
